69eca87954ce4e3c306d6dd91c97ca0b7c652027
S. Mikkel Wilson <swilson@liveops.com>
2014-01-24 10:45:13 -0800

    Menu selection

--- index.js
+++ index.js
@@ -337,25 +337,216 @@ function Elecraft(){
     "IF":   {name:"GeneralInformation",
              description: "General information", 
              parser: function(e){
-               e.frequencyVFOA=14.268000;
+               e.frequencyVFOA=14.268000; // TODO this is obviously wrong
             }},
     "IO":   {description:"Internal Use Only"}, 
-    "IS":   {description:"IF shift"}, 
-    "K2":   {description:"K2 command mode"}, 
-    "K3":   {description:"K3 command mode"}, 
-    "KS":   {description:"Keyer speed"}, 
+    "IS":   {name:"IFShift",
+             description:"IF shift", 
+             parser: function(e){
+               e.IFShift = e.data..substr(2).trimLeft();
+            }},
+    "K2":   {name:"k2Mode",
+             description:"K2 command mode", 
+             parser: function(e){
+               e.k2mode = parseInt(e.data..substr(2));
+            }},
+    "K3":   {name:"k3mode",
+             description:"K3 command mode", 
+             parser: function(e){
+               e.k2mode = parseInt(e.data..substr(2));
+            }},
+    "KS":   {name:"keyerSpeed",
+             description:"Keyer speed", 
+             parser: function(e){
+               e.keyerSpeed = parseInt(e.data..substr(2));
+            }},
     "KT":   {description:"Internal Use Only"}, 
-    "KY":   {description:"Keyboard CW/DATA"}, 
+    "KY":   {name:"CWData",
+             description:"Keyboard CW/DATA", 
+             parser: function(e){
+               e.CWData = e.data.substr(2).leftTrim();
+            }},
     "LD":   {description:"Internal Use Only"}, 
-    "LK":   {description:"VFO-A Lock"}, 
-    "LK$":  {description:"VFO-B Lock"}, 
-    "LN":   {description:"Link VFOs"}, 
-    "MC":   {description:"Memory channel"}, 
-    "MD":   {description:"Operating Mode VFO-A"}, 
-    "MD$":  {description:"Operating Mode VFO-B"}, 
-    "MG":   {description:"Mic gain"}, 
-    "ML":   {description:"Monitor level"}, 
-    "MN":   {description:"Menu entry number"}, 
+    "LK":   {name:"lockVFOA",
+             description:"VFO-A Lock", 
+             parser: function(e){
+               var lock = e.data.substr(2);
+               e.lockVFOA = lock=='1'?true:false;
+            }},
+    "LK$":  {name:"lockVFOB",
+             description:"VFO-B Lock", 
+             parser: function(e){
+               var lock = e.data.substr(2);
+               e.lockVFOB = lock=='1'?true:false;
+            }},
+    "LN":   {name:"linkVFOs",
+             description:"Link VFOs", 
+             parser: function(e){
+               var link = e.data.substr(2);
+               e.linkVFOs = link=='1'?true:false;
+            }},
+    "MC":   {name:"memoryChannel",
+             description:"Memory channel", 
+             parser: function(e){
+               e.memoryChannel = parseInt(e.data.substr(2));
+            }},
+    "MD":   {name:"operatingModeVFOA",
+             description:"Operating Mode VFO-A", 
+             parser: function(e){
+               e.operatingModeVFOA = operatingMode(parseInt(e.data.substr(2)));
+            }},
+    "MD$":  {name:"operatingModeVFOB",
+             description:"Operating Mode VFO-B", 
+             parser: function(e){
+               e.operatingModeVFOB = operatingMode(parseInt(e.data.substr(2)));
+            }},
+    "MG":   {name:"micGain",
+             description:"Mic gain", 
+             parser: function(e){
+               e.micGain = parseInt(e.data.substr(2));
+            }},
+    "ML":   {name:"monitorLevel",
+             description:"Monitor level", 
+             parser: function(e){
+               e.micGain = parseInt(e.data.substr(2));
+            }},
+    "MN":   {name:"menuSelection",
+             description:"Menu entry number", 
+             parser: function(e){
+               // TODO this is big. Every menu item on the K3
+               var menuItem = parseInt(e.data.substr(2));
+               switch(menuItem){
+                 case 0: e.menuItem = "ALARM"; break;
+                 case 1: e.menuItem = "IAMBIC"; break;
+                 case 2: e.menuItem = "LCD ADJ"; break;
+                 case 3: e.menuItem = "LCD BRT"; break;
+                 case 4: e.menuItem = "LED BRT"; break;
+                 case 5: e.menuItem = "MSG RPT"; break;
+                 case 6: e.menuItem = "PADDLE"; break;
+                 case 7: e.menuItem = "RPT OFS"; break;
+                 case 8: e.menuItem = "RX EQ"; break;
+                 case 9: e.menuItem = "TX EQ"; break;
+
+                 case 10: e.menuItem = "VOX GN"; break;
+                 case 11: e.menuItem = "ANTIVOX"; break;
+                 case 12: e.menuItem = "WEIGHT"; break;
+                 case 13: e.menuItem = "2 TONE"; break;
+                 case 14: e.menuItem = "AFV TIM"; break;
+                 case 15: e.menuItem = "MIC+LIN"; break;
+                 case 16: e.menuItem = "TX DLY"; break;
+                 case 17: e.menuItem = "AGC SLP"; break;
+                 case 18: e.menuItem = "FM MODE"; break;
+                 case 19: e.menuItem = "DIGOUT1"; break;
+
+                 case 20: e.menuItem = "AGC HLD"; break;
+                 case 21: e.menuItem = "FM DEV"; break;
+                 case 22: e.menuItem = "EXT ALC"; break;
+                 case 23: e.menuItem = "KAT3"; break;
+                 case 24: e.menuItem = "BAT MIN"; break;
+                 case 25: e.menuItem = "TX INH"; break;
+                 case 26: e.menuItem = "SER NUM"; break;
+                 case 27: e.menuItem = "TXG VCE"; break;
+                 case 28: e.menuItem = "FW REVS"; break;
+                 case 29: e.menuItem = "DATE"; break;
+
+                 case 30: e.menuItem = "DATE MD"; break;
+                 case 31: e.menuItem = "DDS FRQ"; break;
+                 case 32: e.menuItem = "LIN OUT"; break;
+                 case 33: e.menuItem = "KIO3"; break;
+                 case 34: e.menuItem = "ADC REF"; break;
+                 case 35: e.menuItem = "RFI DET"; break;
+                 case 36: e.menuItem = "KDVR3"; break;
+                 case 37: e.menuItem = "AGC-S"; break;
+                 case 38: e.menuItem = "FLx BW"; break;
+                 case 39: e.menuItem = "FLx FRQ"; break;
+
+                 case 40: e.menuItem = "FLx GN"; break;
+                 case 41: e.menuItem = "FLx ON"; break;
+                 case 42: e.menuItem = "FLTX md"; break;
+                 case 43: e.menuItem = "FP TEMP"; break;
+                 case 44: e.menuItem = "FSK POL"; break;
+                 case 45: e.menuItem = "AUTOINF"; break;
+                 case 46: e.menuItem = "KBPF3"; break;
+                 case 47: e.menuItem = "AF LIM"; break;
+                 case 48: e.menuItem = "KNB3"; break;
+                 case 49: e.menuItem = "AF LIM"; break;
+
+                 case 50: e.menuItem = "KRX3"; break;
+                 case 51: e.menuItem = "KXV3"; break;
+                 case 52: e.menuItem = "LCD TST"; break;
+                 case 53: e.menuItem = "MIC SEL"; break;
+                 case 54: e.menuItem = "NB SAVE"; break;
+                 case 55: e.menuItem = "KPA3"; break;
+                 case 56: e.menuItem = "PA TEMP"; break;
+                 case 57: e.menuItem = "RS232"; break;
+                 case 58: e.menuItem = "TUN PWR"; break;
+                 case 59: e.menuItem = "SYNC DT"; break;
+
+                 case 60: e.menuItem = "SMTR MD"; break;
+                 case 61: e.menuItem = "AGC-F"; break;
+                 case 62: e.menuItem = "REF CAL"; break;
+                 case 63: e.menuItem = "SQ MIN"; break;
+                 case 64: e.menuItem = "SQ SUB"; break;
+                 case 65: e.menuItem = "SMTR OF"; break;
+                 case 66: e.menuItem = "SMTR SC"; break;
+                 case 67: e.menuItem = "SMTR PK"; break;
+                 case 68: e.menuItem = "SPLT SV"; break;
+                 case 69: e.menuItem = "SPKRS"; break;
+
+                 case 70: e.menuItem = "SW TEST"; break;
+                 case 71: e.menuItem = "SW TONE"; break;
+                 case 72: e.menuItem = "TECH MD"; break;
+                 case 73: e.menuItem = "TIME"; break;
+                 case 74: e.menuItem = "AGC THR"; break;
+                 case 75: e.menuItem = "PTT RLS"; break;
+                 case 76: e.menuItem = "BND MAP"; break;
+                 case 77: e.menuItem = "TTY LTR"; break;
+                 case 78: e.menuItem = "TX ALC"; break;
+                 case 79: e.menuItem = "TXGN pwr"; break;
+
+                 case 80: e.menuItem = "SUB AF"; break;
+                 case 81: e.menuItem = "PWR SET"; break;
+                 case 82: e.menuItem = "MIC BTN"; break;
+                 case 83: e.menuItem = "VCO MD"; break;
+                 case 84: e.menuItem = "VFO CTS"; break;
+                 case 85: e.menuItem = "VFO FST"; break;
+                 case 86: e.menuItem = "VFO IND"; break;
+                 case 87: e.menuItem = "VFO OFS"; break;
+                 case 88: e.menuItem = "WMTR pwr"; break;
+                 case 89: e.menuItem = "XVx ON"; break;
+
+                 case 90: e.menuItem = "XVx RF"; break;
+                 case 91: e.menuItem = "XVx IF"; break;
+                 case 92: e.menuItem = "XVx PWR"; break;
+                 case 93: e.menuItem = "XVx OFS"; break;
+                 case 94: e.menuItem = "XVx ADR"; break;
+                 case 95: e.menuItem = "AF GAIN"; break;
+                 case 96: e.menuItem = "TX ESSB"; break;
+                 case 97: e.menuItem = "PSKR+PH"; break;
+                 case 98: e.menuItem = "VFO B->A"; break;
+                 case 99: e.menuItem = "AGC PLS"; break;
+
+                 case 100: e.menuItem = "RIT CLR"; break;
+                 case 101: e.menuItem = "TX GATE"; break;
+                 case 102: e.menuItem = "MEM 0-9"; break;
+                 case 103: e.menuItem = "PTT KEY"; break;
+                 case 104: e.menuItem = "VFO CRS"; break;
+                 case 105: e.menuItem = "AFX MD"; break;
+                 case 106: e.menuItem = "SIG RMV"; break;
+                 case 107: e.menuItem = "AFSK TX"; break;
+                 case 108: e.menuItem = "AGC DCY"; break;
+                 case 109: e.menuItem = "PB CTRL"; break;
+
+                 case 110: e.menuItem = "MACRO x"; break;
+                 case 111: e.menuItem = "L-MIX-R"; break;
+                 case 112: e.menuItem = "CW QRQ"; break;
+                 case 113: e.menuItem = "TX DVR"; break;
+                 case 114: e.menuItem = "TX MON"; break;
+                 case 115: e.menuItem = "DUAL PB"; break;
+                           
+                 default: e.menuItem = "Exit Menu";
+               };
+            }},
     "MP":   {description:"Menu param read/set"}, 
     "MQ":   {description:"Menu param read/set"}, // KX3 only
     "NB":   {description:"Noise Blanketer VFO-A"}, 
@@ -401,6 +592,27 @@ function Elecraft(){
     "XF":   {description:"XFIL number VFO-A"}, 
     "XF$":  {description:"XFIL number VFO-B"}, 
     "XT":   {description:"XIT on/off"}
+  }
+
+  function operatingMode(e){
+    switch(e){
+      case 1:
+        return 'LSB';
+      case 2:
+        return 'USB';
+      case 3:
+        return 'CW';
+      case 4:
+        return 'FM';
+      case 5:
+        return 'AM';
+      case 6:
+        return 'DATA';
+      case 7:
+        return 'CW-REV';
+      case 9:
+        return 'DATA-REV';
+    }
   }
 
   function bandChangeUpDown(e){
